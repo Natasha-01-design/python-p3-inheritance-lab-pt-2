b456c0b125fdf1c08b27723ac596dc013e4f56a2
Natasha-01-design <natashakarwitha6@gmail.com>
2025-08-12 11:29:56 +0300

    corrected

--- lib/student.py
+++ lib/student.py
@@ -5,12 +5,13 @@ class Student:
         print("Pick me!")
 
 class ChattyStudent(Student):
-    def __init__(self):
+    def hello(self):
          super().hello()
-         super().raise_hand()
+         
          print("How are you doing today? I'm okay," \
          " but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers?" \
          " Okay well let me just tell you who died...")
-         print_me="pick me"* 10
-         print(print_me)
+    def raise_hand(self):
+         for i in range(10):
+          super().raise_hand()    
     
